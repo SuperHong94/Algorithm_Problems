83a72a703e72dfe1d7dabed802dad99e6e0b3087
SuperHong94 <ghdtnswh213@gmail.com>
2021-09-19 16:06:49 +0900

    fix to memoryzation 시간 초과 문제 때문에 메모리 제이션 기법을 사용하였다. 시간 초과는 통과하지만 정답이 나오지 않는다. 다시 풀어보자.

--- BOGGLE/BOGGLE/main.cpp
+++ BOGGLE/BOGGLE/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <memory.h>
 
 using namespace std;
 int solution(vector<string> inputs, string word, int x, int y, int wordN);
-bool flag = false; 
+bool memo[5][5][10];
+bool flag = false;
 void F(vector<string> inputs, string word)
 {
 	flag = false;
+	memset(memo, true, sizeof(memo));
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < 5; ++j) {
 			int r = 0;
 			r = solution(inputs, word, j, i, 0);
 			if (r != 0)
 			{
-				cout << r;
 				cout << word << " YES\n";
 				return;
 			}
@@ -24,10 +26,11 @@ void F(vector<string> inputs, string word)
 }
 int solution(vector<string> inputs, string word, int x, int y, int wordN)
 {
-	if (wordN == word.size() - 1){
+	if (wordN >= word.size()) {
 		flag = true;
 		return 1;
 	}
+
 	if (x < 0 || x>5 || y < 0 || y>5)
 		return 0;
 	if (inputs[y][x] != word[wordN])
@@ -35,37 +38,67 @@ int solution(vector<string> inputs, string word, int x, int y, int wordN)
 	else {
 		int result = 0;
 		if (x - 1 >= 0 && y - 1 >= 0) //ÁÂ»ó
-		{ 
-			result += solution(inputs, word, x - 1, y - 1, wordN + 1);
+		{
+			if (memo[y - 1][x - 1][wordN + 1]) {
+				result += solution(inputs, word, x - 1, y - 1, wordN + 1);
+				memo[y - 1][x - 1][wordN + 1] = result;
+			}
 			if (flag)
 				return result;
 		}
-		if (y - 1 >= 0) //»ó
-			result += solution(inputs, word, x, y - 1, wordN + 1);
+		if (memo[y - 1][x][wordN + 1]) {
+			if (y - 1 >= 0) //»ó
+				result += solution(inputs, word, x, y - 1, wordN + 1);
+			memo[y - 1][x][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (x + 1 < 5 && y - 1 >= 0) //¿ì»ó
-			result += solution(inputs, word, x + 1, y - 1, wordN + 1);
+
+		if (memo[y - 1][x + 1][wordN + 1]) {
+			if (x + 1 < 5 && y - 1 >= 0) //¿ì»ó
+				result += solution(inputs, word, x + 1, y - 1, wordN + 1);
+			memo[y - 1][x + 1][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (x - 1 >= 0) //ÁÂ
-			result += solution(inputs, word, x - 1, y, wordN + 1);
+
+		if (memo[y][x - 1][wordN + 1]) {
+			if (x - 1 >= 0) //ÁÂ
+				result += solution(inputs, word, x - 1, y, wordN + 1);
+			memo[y][x - 1][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (x + 1 < 5) //¿ì
-			result += solution(inputs, word, x + 1, y, wordN + 1);
+
+		if (memo[y][x + 1][wordN + 1]) {
+			if (x + 1 < 5) //¿ì
+				result += solution(inputs, word, x + 1, y, wordN + 1);
+			memo[y][x + 1][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (x - 1 >= 0 && y + 1 < 5) //ÁÂÇÏ
-			result += solution(inputs, word, x - 1, y + 1, wordN + 1);
+
+		if (memo[y + 1][x - 1][wordN + 1]) {
+			if (x - 1 >= 0 && y + 1 < 5) //ÁÂÇÏ
+				result += solution(inputs, word, x - 1, y + 1, wordN + 1);
+			memo[y + 1][x - 1][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (y + 1 < 5) //ÇÏ
-			result += solution(inputs, word, x, y +1, wordN + 1);
+
+		if (memo[y + 1][x][wordN + 1]) {
+			if (y + 1 < 5) //ÇÏ
+				result += solution(inputs, word, x, y + 1, wordN + 1);
+			memo[y + 1][x][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
-		if (x + 1 < 5 && y + 1 < 5) //¿ìÇÏ
-			result += solution(inputs, word, x + 1, y + 1, wordN + 1);
+
+		if (memo[y + 1][x + 1][wordN + 1]) {
+			if (x + 1 < 5 && y + 1 < 5) //¿ìÇÏ
+				result += solution(inputs, word, x + 1, y + 1, wordN + 1);
+			memo[y + 1][x + 1][wordN + 1] = result;
+		}
 		if (flag)
 			return result;
 		return result;
@@ -89,9 +122,12 @@ int main()
 		cin >> N;
 		for (int k = 0; k < N; ++k) {
 			cin >> knownWord[k];
-			F(inputs, knownWord[k]);
+
 		}
 
+		for (int o = 0; o < N; ++o) {
+			F(inputs, knownWord[o]);
+		}
 
 	}
 }
