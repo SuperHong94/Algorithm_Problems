0eeefabb302e394044c5ee06d2b8256799209953
SuperHong94 <ghdtnswh213@gmail.com>
2021-09-21 18:03:59 +0900

    fix/complete BOGGLE 동적 계획법으로 처음부터 구현 완료

--- BOGGLE/BOGGLE/main.cpp
+++ BOGGLE/BOGGLE/main.cpp
@@ -2,132 +2,74 @@
 #include <vector>
 #include <string>
 #include <memory.h>
+#include <array>
 
 using namespace std;
-int solution(vector<string> inputs, string word, int x, int y, int wordN);
-bool memo[5][5][10];
-bool flag = false;
-void F(vector<string> inputs, string word)
+
+char words[5][5];
+bool memory[5][5][10];
+constexpr int dx[8] = { -1,0,1,-1,1,-1,0,1 };
+constexpr int dy[8] = { -1,-1,-1,0,0,1,1,1 };
+
+bool findWord(const string& word, int y, int x, int wordN)
 {
-	flag = false;
-	memset(memo, true, sizeof(memo));
+	
+	if (x < 0 || y < 0 || x >=5 || y >= 5)
+		return false;
+	memory[y][x][wordN] = false; //이미 방문
+	if (words[y][x] != word[wordN]) {
+		return false;
+	}
+
+	if (word.size()-1 == wordN)
+		return true;
+	for (int i = 0; i < 8; ++i) {
+		if (memory[y + dy[i]][x + dx[i]][wordN + 1])
+			if (findWord(word, y + dy[i], x + dx[i], wordN + 1))
+				return true;
+	}
+	return false;
+}
+
+bool solution(const string& word)
+{
+
+	memset(memory, true, sizeof(memory));
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < 5; ++j) {
-			int r = 0;
-			r = solution(inputs, word, j, i, 0);
-			if (r != 0)
-			{
-				cout << word << " YES\n";
-				return;
-			}
+			if (findWord(word, i, j, 0))
+				return true;
 		}
 	}
-	cout << word << " NO\n";
+	return false;
 }
-int solution(vector<string> inputs, string word, int x, int y, int wordN)
-{
-	if (wordN >= word.size()) {
-		flag = true;
-		return 1;
-	}
-
-	if (x < 0 || x>5 || y < 0 || y>5)
-		return 0;
-	if (inputs[y][x] != word[wordN])
-		return 0;
-	else {
-		int result = 0;
-		if (x - 1 >= 0 && y - 1 >= 0) //좌상
-		{
-			if (memo[y - 1][x - 1][wordN + 1]) {
-				result += solution(inputs, word, x - 1, y - 1, wordN + 1);
-				memo[y - 1][x - 1][wordN + 1] = result;
-			}
-			if (flag)
-				return result;
-		}
-		if (memo[y - 1][x][wordN + 1]) {
-			if (y - 1 >= 0) //상
-				result += solution(inputs, word, x, y - 1, wordN + 1);
-			memo[y - 1][x][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y - 1][x + 1][wordN + 1]) {
-			if (x + 1 < 5 && y - 1 >= 0) //우상
-				result += solution(inputs, word, x + 1, y - 1, wordN + 1);
-			memo[y - 1][x + 1][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y][x - 1][wordN + 1]) {
-			if (x - 1 >= 0) //좌
-				result += solution(inputs, word, x - 1, y, wordN + 1);
-			memo[y][x - 1][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y][x + 1][wordN + 1]) {
-			if (x + 1 < 5) //우
-				result += solution(inputs, word, x + 1, y, wordN + 1);
-			memo[y][x + 1][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y + 1][x - 1][wordN + 1]) {
-			if (x - 1 >= 0 && y + 1 < 5) //좌하
-				result += solution(inputs, word, x - 1, y + 1, wordN + 1);
-			memo[y + 1][x - 1][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y + 1][x][wordN + 1]) {
-			if (y + 1 < 5) //하
-				result += solution(inputs, word, x, y + 1, wordN + 1);
-			memo[y + 1][x][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-
-		if (memo[y + 1][x + 1][wordN + 1]) {
-			if (x + 1 < 5 && y + 1 < 5) //우하
-				result += solution(inputs, word, x + 1, y + 1, wordN + 1);
-			memo[y + 1][x + 1][wordN + 1] = result;
-		}
-		if (flag)
-			return result;
-		return result;
-	}
-}
-
 int main()
 {
-	vector<string> inputs;
-	int n = 0;
-	cin >> n;
+	int C = 0;
+	cin >> C;
 
-	int N = 0;
-	string knownWord[10];
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			string tmp;
-			cin >> tmp;
-			inputs.emplace_back(tmp);
+
+	for (int caseN = 0; caseN < C; ++caseN) {
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+				cin >> words[i][j];
+			}
 		}
+
+		int N = 0;//알고있는 단어의 수
 		cin >> N;
-		for (int k = 0; k < N; ++k) {
-			cin >> knownWord[k];
+		string arr[10];
+		for (int i = 0; i < N; ++i) {
+			cin >> arr[i];
 
 		}
 
-		for (int o = 0; o < N; ++o) {
-			F(inputs, knownWord[o]);
+		for (int i = 0; i < N; ++i) {
+			cout << arr[i];
+			if (solution(arr[i]))
+				cout << " YES\n";
+			else
+				cout << " NO\n";
 		}
-
 	}
 }
