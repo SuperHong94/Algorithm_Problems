2585c5373c12533fa54080c7d9a15e8bcf0c521c
SuperHong94 <ghdtnswh213@gmail.com>
2021-09-24 23:37:28 +0900

    dd

--- algospot_PICNIC/algospot_PICNIC/소스.cpp
+++ algospot_PICNIC/algospot_PICNIC/소스.cpp
@@ -3,36 +3,25 @@
 #include <memory.h>
 using namespace std;
 
-bool person[10];
-int serach(const vector < vector<int>>& v,int y,int cnt, int n)
+bool couples[10][10];
+bool people[10];
+int search(int n, int cnt)
 {
-	if (cnt == n)
+	if (cnt >= n)
 		return 1;
-	for (int i = y; i < n; ++i) {
-		for(int j=0;j<v[i].size();++j)
-		if (person[i] == false&&person[j]==false) {
-			person[i] = true;
-			person[j] = true;
-			return serach(v, i, cnt + 2, n);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
 		}
 	}
-	return 0;
 }
-int solution(vector < vector<int>>& v,const int n)
+int solution(int n)
 {
-	int result = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < v[i].size(); ++j) {
-			memset(person, false, sizeof(person));
-			person[i] = true;
-			person[v[i][j]] = true;
-			v[i][j] = -1;
-			v[v[i][j]].erase(v[v[i][j]].begin() + i);
-			result += serach(v, i, 2, n);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+
 		}
 	}
-	return result;
+	
 }
 int main()
 {
@@ -43,16 +32,14 @@ int main()
 		cin >> n;
 		int m = 0;  //친구 쌍의 수
 		cin >> m;
-		vector<vector<int>> v;
-		v.resize(n);
+		memset(couples, false, sizeof(couples));
 		for (int j = 0; j < m; ++j) {
 			int a = 0, b = 0;
 			cin >> a >> b;
-			v[a].push_back(b); //a의 친구 b 추가
-			v[b].push_back(a); //b의 친구 a 추가
-
+			couples[a][b] = true;
+			couples[b][a] = true;
 		}
-		cout << solution(v, n) << endl;
+		int res = solution(n);
 
 	}
 }
