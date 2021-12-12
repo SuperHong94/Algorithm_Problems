672159bf54e5b4add7c338e980da62f6ac3e614b
SuperHong94 <ghdtnswh213@gmail.com>
2021-12-12 20:24:17 +0900

    RGB거리

--- RGB거리_ 백준/RGB거리_ 백준/소스.cpp
+++ RGB거리_ 백준/RGB거리_ 백준/소스.cpp
@@ -2,16 +2,57 @@
 
 using namespace std;
 
-int R[1000s], G[1000], B[1000];
-
-int F(int n)
+enum COLOR :int
 {
-	R
+	RED = 0, BLUE, GREEN
+};
+
+int RGB[3][1000];
+int cache[3][1000];
+
+int F(int n, COLOR cur)
+{
+	if (n == -1)
+		return 0;
+	if (cache[cur][n] != 0)
+		return cache[cur][n];
+	switch (cur)
+	{
+	case RED: {
+		int B = F(n - 1, BLUE);
+		int G = F(n - 1, GREEN);
+		return B < G ? cache[cur][n] = RGB[RED][n] + B : cache[cur][n] = RGB[RED][n] + G;
+		break;
+	}
+	case BLUE:
+	{
+		int R = F(n - 1, RED);
+		int G = F(n - 1, GREEN);
+		return R < G ? cache[cur][n] = RGB[BLUE][n] + R : cache[cur][n] = RGB[BLUE][n] + G;
+		break;
+	}
+	case GREEN:
+	{
+		int R = F(n - 1, RED);
+		int B = F(n - 1, BLUE);
+		return R < B ? cache[cur][n] = RGB[GREEN][n] + R : cache[cur][n] = RGB[GREEN][n] + B;
+		break;
+	}
+
+	}
 }
 int main()
 {
 	int n = 0; cin >> n;
 	for (int i = 0; i < n; ++i) {
-		cin >> R[i] >> G[i] >> B[i];
+		cin >> RGB[RED][i] >> RGB[BLUE][i] >> RGB[GREEN][i];
 	}
+
+	int MIN = 9999999;
+	for (int i = 0; i < 3; ++i)
+	{
+		F(n - 1, (COLOR)i);
+		MIN = min(MIN, cache[(COLOR)(i)][n - 1]);
+	}
+	cout << MIN;
 }
