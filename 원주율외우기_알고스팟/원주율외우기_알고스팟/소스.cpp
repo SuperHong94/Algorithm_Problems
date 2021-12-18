6a794feac193c59ac03332c038499bf8dce43fb7
SuperHong94 <ghdtnswh213@gmail.com>
2021-12-18 09:16:04 +0900

    git pull

---
+++ 원주율외우기_알고스팟/원주율외우기_알고스팟/소스.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char cache[10001];
+int solv(const string& s)
+{
+	//진짜 모르겠넹 ㅜㅜ 아버지! 정답을 알려줘 나는 어리고 어려 
+}
+
+
+int main()
+{
+	int c; cin >> c;
+
+	while (c--)
+	{
+		string s;
+		cin >> s;
+
+	}
+}
