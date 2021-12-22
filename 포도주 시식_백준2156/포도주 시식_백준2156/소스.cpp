790d8e8c954f43200638a95f43fcb53352a80c0a
SuperHong94 <ghdtnswh213@gmail.com>
2021-12-22 23:13:18 +0900

    백준 포도주 마시기 완료

--- 포도주 시식_백준2156/포도주 시식_백준2156/소스.cpp
+++ 포도주 시식_백준2156/포도주 시식_백준2156/소스.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 
-int wine[10000];
-int cache[10000];
+int wine[10002];
+int cache[10002];
 int N;
 
 
@@ -15,17 +15,31 @@ int solv(int start)
 	if (cache[start] != 0)
 		return cache[start];
 
-	return cache[start] =max(wine[start] + solv(start + 2), wine[start] + solv(start + 1));
+
+	int v1 = wine[start] + wine[start+1]+solv(start+3);
+	int v3 = wine[start + 1] + wine[start + 2] + solv(start + 4);
+	int v2 = wine[start] + solv(start + 2);
+	
+
+	
+	return cache[start]=max(max(v1,v2),v3);
 
 }
 
 int main()
 {
 	cin >> N;
-	for (int i = 0; i < N; ++i)
+	for (int i = 1; i <= N; ++i)
 		cin >> wine[i];
 
+	cache[1] = wine[1];
+	cache[2] = wine[1] + wine[2];
 
-	cout << solv(0);
+	for (int i = 3; i <= N; ++i)
+	{
+		cache[i] = max(cache[i - 1], max(cache[i - 2] + wine[i], cache[i - 3] + wine[i - 1] + wine[i]));
+	}
+
+	cout << cache[N];
 
 }
