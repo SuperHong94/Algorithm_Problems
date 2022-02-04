75e4789709574ca8e7304fd8ab6b0ce81ab6e65d
SuperHong94 <ghdtnswh213@gmail.com>
2022-02-04 22:48:13 +0900

    완료

--- 숫자 문자열과 영단어_programmers/숫자 문자열과 영단어_programmers/main.cpp
+++ 숫자 문자열과 영단어_programmers/숫자 문자열과 영단어_programmers/main.cpp
@@ -6,25 +6,50 @@ using namespace std;
 
 const string numbers[10] = { "zero","one","two","three","four","five","six","seven","eight","nine" };
 
-string answer = "";
+
+
+char StringToNumber(const string& s, int& index)
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        int numlength = numbers[i].length();
+        if (s.length() < numlength)continue;
+        if (s.substr(0, numlength) == numbers[i]) {
+            index += numlength-1;
+            return i + '1'-1;
+        }
+    }
+}
 int solution(string s) {
-    int answer = 0;
+    string answer="";
     for (int i = 0; i < s.length(); ++i) {
         if (0 == isalpha(s[i])) {
             answer += s[i];
         }
         else {
-            for (int j = 0; j < 10; ++j) {
-
-            }
+            answer+=StringToNumber(s.substr(i), i);
         }
     }
  
-    return answer;
+    return atoi(answer.data());
 }
 
 
-int main()
-{
-    solution("s");
+
+
+//심박한 풀이
+#include <regex>
+
+int solution2(string s) {
+    s = regex_replace(s, regex("zero"), "0");
+    s = regex_replace(s, regex("one"), "1");
+    s = regex_replace(s, regex("two"), "2");
+    s = regex_replace(s, regex("three"), "3");
+    s = regex_replace(s, regex("four"), "4");
+    s = regex_replace(s, regex("five"), "5");
+    s = regex_replace(s, regex("six"), "6");
+    s = regex_replace(s, regex("seven"), "7");
+    s = regex_replace(s, regex("eight"), "8");
+    s = regex_replace(s, regex("nine"), "9");
+    return stoi(s);
 }
