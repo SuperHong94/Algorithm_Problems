f1901d886066797eb8f2906c8ddd229225bd7025
SuperHong94 <ghdtnswh213@gmail.com>
2022-02-09 22:52:50 +0900

    백준 쉬운계단수 슬라이딩 윈도 기법

--- 쉬운계단수_BJ10844/쉬운계단수_BJ10844/main.cpp
+++ 쉬운계단수_BJ10844/쉬운계단수_BJ10844/main.cpp
@@ -2,13 +2,24 @@
 #include <math.h>
 using namespace std;
 
-
+int cache[2][12];
 int main()
 {
 	int N = 0; cin >> N;
-
-	for (int i = 3; i <= N; ++i)
+	int answer = 9;
+	int mod = 1000'000'000;
+	for (int i = 2; i < 11; ++i)
+		cache[1][i] = 1;
+	for (int i = 2; i <= N; ++i)
 	{
+		answer = 0;
+
+        for(int j = 1; j < 11; j++) {
+			cache[i%2][j] = (cache[(i-1)%2][j-1] + cache[(i-1)%2][j+1]) % mod;
+			answer = (answer + cache[i%2][j]) % mod;
+        }
 
 	}
+
+	cout << answer << '\n';
 }
