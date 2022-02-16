dca60a10202dd1fd0a3e95292e35fee3735e9d09
SuperHong94 <ghdtnswh213@gmail.com>
2022-02-16 23:45:20 +0900

    치즈 도전주

--- 치즈_bj2638/치즈_bj2638/main.cpp
+++ 치즈_bj2638/치즈_bj2638/main.cpp
@@ -5,15 +5,47 @@ using namespace std;
 
 int Board[101][101];
 int cache[101][101];
-int y = 0, x = 0;
+const int dy[] = { -1,1,0,0 };
+const int dx[] = { 0,0,-1,1 };
+int Y = 0, X = 0;
 
+int DFS(int y, int x)
+{
+	if (cache[y][x] != -1)
+		return cache[y][x];
+	if (-1 == Board[y][x])
+		return 0;//삭제예정
+	if (1 == Board[y][x]||2 == Board[y][x])
+		return 1;
+
+	for (int i = 0; i < 4; ++i) {
+		if (DFS(dy[i] + y, dx[i] + x) == 0)
+			return cache[y][x] = 0;
+	}
+	return cache[y][x] = 1;
+
+}
+
+void solution()
+{
+	memset(cache, -1, sizeof(cache));
+
+	if (DFS(1,1) == 0) {
+						Board[i][j] = 2;
+						break;
+					}
+
+
+}
 int main()
 {
 	memset(Board, -1, sizeof(Board)); //맨 가생이는 -1로 할꺼임
-	cin >> y >> x;
+	cin >> Y >> X;
 
-	for (int i = 1; i <= y; ++i)
-		for (int j = 1; j <= x; ++j)
+	for (int i = 1; i <= Y; ++i)
+		for (int j = 1; j <= X; ++j)
 			cin >> Board[i][j];
+
+	solution();
 
 }
