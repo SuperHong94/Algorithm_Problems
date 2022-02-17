c36db064b93b8462d85f57599eb2cbc3965174e6
SuperHong94 <ghdtnswh213@gmail.com>
2022-02-18 00:00:03 +0900

    품

--- 치즈_bj2638/치즈_bj2638/main.cpp
+++ 치즈_bj2638/치즈_bj2638/main.cpp
@@ -12,29 +12,46 @@ int Y = 0, X = 0;
 int DFS(int y, int x)
 {
 	if (cache[y][x] != -1)
-		return cache[y][x];
-	if (-1 == Board[y][x])
-		return 0;//삭제예정
-	if (1 == Board[y][x]||2 == Board[y][x])
+		return 0;
+	cache[y][x] = 0; //갔던곳 못감 ㅎㅎ
+	if (Board[y][x] == -1)
+		return 0;
+	if (Board[y][x] == 1 || Board[y][x] == 2) {
+		Board[y][x] = 2;
 		return 1;
-
-	for (int i = 0; i < 4; ++i) {
-		if (DFS(dy[i] + y, dx[i] + x) == 0)
-			return cache[y][x] = 0;
 	}
-	return cache[y][x] = 1;
+
+	for (int i = 0; i < 4; ++i)
+	{
+		DFS(dy[i] + y, dx[i] + x);
+
+	}
 
 }
 
-void solution()
+int GetCheaseCnt()
+{
+	int ret = 0;
+	for (int i = 1; i <= Y; ++i)
+		for (int j = 1; j <= X; ++j) {
+			if (Board[i][j] == 1)
+				ret++;
+		}
+	return ret;
+}
+int solution()
 {
 	memset(cache, -1, sizeof(cache));
 
-	if (DFS(1,1) == 0) {
-						Board[i][j] = 2;
-						break;
-					}
+	int cnt = 0;
+	int cheaseCnt = GetCheaseCnt();
+	while (0 != cheaseCnt)
+	{
+		DFS(1, 1);
+		cnt += 1;
+	}
 
+	return cnt;
 
 }
 int main()
