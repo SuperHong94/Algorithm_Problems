d8d4bcf1a3acdc5efca22337ac807beb40ce1733
SuperHong94 <ghdtnswh213@gmail.com>
2022-02-21 22:26:39 +0900

    다시시도

--- 평범한배낭_bj12865/평범한배낭_bj12865/소스.cpp
+++ 평범한배낭_bj12865/평범한배낭_bj12865/소스.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <memory.h>
 using namespace std;
 
 
@@ -15,27 +16,41 @@ struct Object
 
 
 vector<Object> objects;
-int cache[100][100];// 조합되었는지 안되었는지 확인
+int cache[100][100];//
 
-int DFS(int index)
+/// <summary>
+/// 
+/// </summary>
+/// <param name="index"> 현재 인덱스</param>
+/// <param name="weight"> 지금까지의 무게</param>
+/// <returns></returns>
+int DFS(int index, int weight)
 {
-	cache[index][index] = 1;
-	int ret = 0;
-	for (int i = 0; i < objects.size(); ++i) {
-		if (cache[index][i] == 1) continue;
-		cache[index][i] = 1;  
-		cache[i][index] = 1;
-		if()
+
+	cache[index][index] = objects[index]._value;
+
+	//cache[weight] = objects[index]._value;
+	int ret = objects[index]._value;
+	for (int i = index; i < objects.size(); ++i) {
+		if (cache[index][i] == -1)
+			if (i, weight + objects[i]._weight <= K)
+				cache[index][i] = objects[index]._value + DFS(i, weight + objects[i]._weight);
+		ret = max(cache[index][i], ret);
 	}
+	return ret;
 }
 
 int main()
 {
+	memset(cache, -1, sizeof(cache));
 	cin >> N >> K;
 	for (int i = 0; i < N; ++i) {
 		Object temp;
 		cin >> temp._weight >> temp._value;
 		objects.emplace_back(temp);
 	}
-
+	for (int i = 0; i < N; ++i) {
+		MAX = max(MAX, DFS(i, objects[i]._weight));
+	}
+	cout << MAX << '\n';
 }
