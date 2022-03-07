22c6e25837061fb57bbd50d5b2a691d0ac6c6a72
SuperHong94 <ghdtnswh213@gmail.com>
2022-03-07 22:04:30 +0900

    병범한 배낭 재도전

--- 평범한배낭_bj12865/평범한배낭_bj12865/소스.cpp
+++ 평범한배낭_bj12865/평범한배낭_bj12865/소스.cpp
@@ -1,82 +1,3 @@
-//#include <iostream>
-//#include <vector>
-//#include <memory.h>
-//using namespace std;
-//
-//
-//int N; //물품수
-//int K; //버틸 수 있는 무게
-//
-//
-//struct Object
-//{
-//	int _weight;
-//	int _value;
-//	Object operator+(const Object& right)
-//	{
-//		_weight = _weight + right._weight;
-//		_value = _value + right._value;
-//		return *this;
-//	}
-//	bool operator<(const Object& right) const {
-//		return _value < right._value;
-//	}
-//
-//};
-//
-//
-//vector<Object> objects;
-//Object cache[100];//
-//
-///// <summary>
-///// 
-///// </summary>
-///// <param name="index"> 현재 인덱스</param>
-///// <param name="weight"> 지금까지의 무게</param>
-///// <returns></returns>
-//Object DFS(int index)
-//{
-//	if (cache[index]._value != -1)
-//		//return cache[index];
-//
-//	Object ret = objects[index];
-//
-//	for (int i = 0; i < objects.size(); ++i) {
-//		if (i == index)continue;
-//		Object temp = DFS(i);
-//		if (temp._weight + ret._weight > K)
-//			ret = max(ret, temp);
-//		else
-//			ret = ret + temp;
-//
-//		if (objects[index]._weight + objects[i]._weight > K)continue;
-//		ret = max(ret, objects[index]);
-//
-//
-//
-//	}
-//	return cache[index] = ret;
-//}
-//
-//int main()
-//{
-//	memset(cache, -1, sizeof(cache));
-//	cin >> N >> K;
-//	for (int i = 0; i < N; ++i) {
-//		Object temp;
-//		cin >> temp._weight >> temp._value;
-//		objects.emplace_back(temp);
-//	}
-//	int MAX = 0;
-//	for (int i = 0; i < N; ++i) {
-//		memset(cache, -1, sizeof(cache));
-//		MAX = max(MAX, DFS(i)._value);
-//	}
-//	cout << MAX << '\n';
-//}
-
-
-
 #include <iostream>
 
 #define endl "\n"
@@ -120,11 +41,9 @@ void Solve()
 
 int main(void)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    //freopen("Input.txt", "r", stdin);
+
+
     Solve();
 
     return 0;
