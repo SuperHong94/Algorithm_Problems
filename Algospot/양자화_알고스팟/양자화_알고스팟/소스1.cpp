2a554c53db8388e507089471d6c6d1dacdf83acc
SuperHong94 <ghdtnswh213@gmail.com>
2022-04-04 22:46:51 +0900

    모든 조합출력 완료

--- Algospot/양자화_알고스팟/양자화_알고스팟/소스1.cpp
+++ Algospot/양자화_알고스팟/양자화_알고스팟/소스1.cpp
@@ -12,35 +12,39 @@ using namespace std;
 
 */
 int start = 1;
-int End = 10;
+int End = 5;
 
 vector<int> v;
 int Combination(int s, int len)
 {
-	if (len == 0) {
+	if (v.size()==3) {
 		for (auto d : v)
 			cout << d << ' ';
 		cout << endl;
+
 		return 0;
 	}
 
 
+
 	for (int i = s + 1; i <= End; ++i)
 	{
-		v.push_back(s);
+		v.push_back(i);
 		Combination(i, len - 1);
 		v.pop_back();
 	}
-
 	return 0;
 }
 
-
+//조합을 하기위해서는?
+//
 
 int main()
 {
-	for (int i = start; i <= End - 4; ++i) {
+	for (int i = start; i <= End; ++i) {
 
-		Combination(i, 4);
+		v.push_back(i);
+		Combination(i, 3);
+		v.pop_back();
 	}
 }
