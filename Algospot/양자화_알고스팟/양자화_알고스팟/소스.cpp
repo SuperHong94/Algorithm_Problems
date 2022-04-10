d91744b5f54e39ab9a0e83c1dd4b5e0492d6b998
SuperHong94 <ghdtnswh213@gmail.com>
2022-04-10 23:13:04 +0900

    알고스팟 책보고 풀음

--- Algospot/양자화_알고스팟/양자화_알고스팟/소스.cpp
+++ Algospot/양자화_알고스팟/양자화_알고스팟/소스.cpp
@@ -5,55 +5,59 @@
 using namespace std;
 
 
-int cache[1000];// 첫시작이 n일때의 값을 저장
-int s = 0;
-vector<int> numbers;//길이만큼의 조합 
-int start = 0;
-int End = 0;
-int quant(const vector<int>& numbers, int start)
-{
-	if (cache[start] != -1)
-		return cache[start];
+const int INF = 987654321;
 
-	cache[start] = 2000;
-	//for(int i=num)
+int n;
+int A[101], pSum[101], pSqSum[101];
+
+void precalc() {
+	sort(A, A + n);
+	pSum[0] = A[0];
+	pSqSum[0] = A[0] * A[0];
+	for (int i = 1; i < n; ++i) {
+		pSum[i] = pSum[i - 1] + A[i];
+		pSqSum[i] = pSqSum[i - 1] + A[i] * A[i];
+	}
 }
 
+int minError(int lo, int hi) {
+	int sum = pSum[hi] - (lo == 0 ? 0 : pSum[lo - 1]);
+	int sqSum = pSqSum[hi] - (lo == 0 ? 0 : pSqSum[lo - 1]);
+	int m = int(0.5 + (double)sum / (hi - lo + 1));
+	int ret = sqSum - 2 * m * sum + m * m * (hi - lo + 1);
+	return ret;
+
+}
+int cache[101][11];
+int quantize(int from, int parts) {
+	if (from == n)return 0;
+	if (parts == 0)return INF;
+	int& ret = cache[from][parts];
+	if (ret != -1)return ret;
+	ret = INF;
+
+	for (int partSize = 1; from + partSize <= n; ++partSize)
+		ret = min(ret, minError(from, from + partSize - 1) + quantize(from + partSize, parts - 1));
+
+	return ret;
+}
 
 int main()
 {
 	int c = 0; cin >> c;
 	while (c--)
 	{
+		memset(cache, 0, sizeof(cache));
+		int s = 0;
+		cin >> n>>s;
+		for (int i = 0; i <n; ++i)
+			cin >> A[i];
+
+		precalc();
 		memset(cache, -1, sizeof(cache));
-		int n = 0; cin >> n;
-		 cin >> s;
-		vector<int> numbers;
-		numbers.resize(n);
-		for (auto& n : numbers)
-			cin >> n;
+		cout << quantize(0, s) << endl;
 
-		sort(numbers.begin(), numbers.end());//순서대로 정렬해주기
 
-		//양자화하기
-		//어떻게?
-		//s=가 1이면
-		//number[0]부터 number[n-1]까지 전부 시도하기
-		//s가 2이면
-		//s[0] s[1],  s[0] s[2] ,,,s[0] s[n-1]   n번
-		//s[1] s[2], ,,, s[n-2] s[n-1] n-1번
-		// ,
-		// ,
-		// ,
-		//s[n-2] s[n-1]  1번 
-
-		//근데 number가 중복될 수 있다. 따라서 이미했던거 또할 필요가없다.
-		//이것을 피해야한다. 어떻게? 동적계획으로 갔던것 저장
-		//int cache[1000]// 첫시작이 n일때의 값을 저장 무슨값? 오차제곱의 합의 최소값
-		
-	
-
-		
 
 	}
 }
