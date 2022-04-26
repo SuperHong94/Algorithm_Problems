beae1f9bf52cfd9ca3108c45c2b5984762db097d
SuperHong94 <ghdtnswh213@gmail.com>
2022-04-26 22:44:04 +0900

    전깃줄 클리어

--- BAEKJOON/전깃줄_백준/전깃줄_백준/소스.cpp
+++ BAEKJOON/전깃줄_백준/전깃줄_백준/소스.cpp
@@ -1,2 +1,52 @@
 //정답은 오른쪽 연결된 선에서 가장긴 부분수열 구하는것이다!
 //이걸 토대로 다시해보자
+//
+/*
+내가 못한 이유 규칙을 못찾음 -> 머리속에서 그림을 못그렸다!
+
+*/
+#include <iostream>
+using namespace std;
+
+
+int lines[501];
+int cache[501];
+int N;
+
+int solution(int start)
+{
+	if (cache[start] != 0)
+		return cache[start];
+
+	int& ret = cache[start]=1;
+
+	for (int i = start+1; i < 501; ++i) {
+		if (lines[start] < lines[i]) {
+			ret = max(ret, solution(i)+1);
+		}
+	}
+	return ret;
+}
+
+int main()
+{
+	int a = 0, b = 0;
+	cin >> N;
+	int total = N;
+	while (N--)
+	{
+		cin >> a >> b;
+		lines[a] = b;
+	}
+	//for (int i = 0; i < N; ++i) {
+	//	cin >> lines[i];
+	//}
+	int ret = 0;
+	for (int i = 0; i < 501; ++i)
+	{
+		if (lines[i] != 0)
+			ret = max(ret, solution(i));
+	}
+	cout << total- ret << endl;
+
+}
