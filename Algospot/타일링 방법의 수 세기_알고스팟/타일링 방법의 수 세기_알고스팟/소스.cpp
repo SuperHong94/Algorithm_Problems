6a9de7d0fe6031641c48bb2df5c30c153c6ef941
SuperHong94 <ghdtnswh213@gmail.com>
2022-04-30 16:01:06 +0900

    타일링 해결

--- Algospot/타일링 방법의 수 세기_알고스팟/타일링 방법의 수 세기_알고스팟/소스.cpp
+++ Algospot/타일링 방법의 수 세기_알고스팟/타일링 방법의 수 세기_알고스팟/소스.cpp
@@ -1,41 +1,36 @@
+//https://algospot.com/judge/problem/read/TILING2
 #include <iostream>
-
+#include <memory.h>
 using namespace std;
 
 const long long MOD = 1000000007;
-int board[2][100];
 int cache[2][100];
 int N = 0;
 
-const int dy[] = { 1,0 };
-const int dx[] = { 0,1 };
+
+// ㅁ			ㅁㅁ
+// ㅁ  이거거나  ㅁㅁ  이렇게내 
 int solution(int y, int x)
 {
 	if (cache[y][x] != 0) {
-		return cache[y][x];
+		return cache[y][x]%MOD;
 	}
 
-	bool flag = true;
-	for (int i = 0; i < 2; ++i)
-		for (int j = 0; j < N; ++j) {
-			if (board[i][j] == 0) {
-				flag = false;
-				break;
-			}
-		}
 
-	if (flag == true)
-		return cache[y][x] = 1;
+	if (x + 1 < N)
+		cache[y][x] += solution(y, x + 1);
+	else
+		return cache[y][x] += 1;
 
-	for (int i = 0; i < 2; ++i) {
-		if (y + dy[i] > 1 || x + dx[i] >= N)continue;
-		board[y][x] = 1;  //지나간 곳 표시
-		board[y + dy[i]][x + dx[i]] = 1;
-		cache[y][x] += solution(y + dy[i], x + dx[i]);
-		board[y][x] = 0;
-		board[y + dy[i]][x + dx[i]] = 0;
-	}
-	return cache[y][x];
+
+
+	if (x + 2 < N)
+		cache[y][x] += solution(y, x + 2);
+	else
+		return cache[y][x] += 1;
+
+
+	return cache[y][x]%MOD;
 }
 int main()
 {
@@ -45,11 +40,21 @@ int main()
 	{
 		cin >> N;
 		memset(cache, 0, sizeof(cache));
-		memset(board, 0, sizeof(board));
 		cout << solution(0, 0) << endl;
 	}
 }
 
 
 //색칠하고 다음스텝으로 넘어가는 거 잘못되었음
-//안칠해지는 경우는 없다.
+//안칠해지는 경우는 없다.]
+
+
+//책의 정답
+int memo[101];
+int tiling(int width)
+{
+	if (width <= 1) return 1;
+	int& ret = memo[width];
+	if (ret != -1)return ret;
+	return ret = (tiling(width - 2) + tiling(width - 1)) % MOD;
+}
