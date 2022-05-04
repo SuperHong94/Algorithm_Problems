61341b4b1a8cd18c5c1506b0e2e9adf224aaf511
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-04 22:34:23 +0900

    다시 시도

--- BAEKJOON/가장 긴 바이토닉 부분 수열_백준/가장 긴 바이토닉 부분 수열_백준/main.cpp
+++ BAEKJOON/가장 긴 바이토닉 부분 수열_백준/가장 긴 바이토닉 부분 수열_백준/main.cpp
@@ -7,13 +7,13 @@ int arr[1000];
 int cache[1000];
 int N = 0;
 
-int solution(int start, bool order)
+int solution(int start)
 {
 	int& ret = cache[start];
 	if (ret != 0)
 		return ret;
 
-	ret = 1; //자기자신
+
 	for (int i = start + 1; i < N; ++i) {
 		if (true == order) {
 			if (arr[start] < arr[i])
@@ -23,6 +23,17 @@ int solution(int start, bool order)
 			ret = max(ret, solution(i, false) + 1);
 
 	}
+	//int left =1;
+	//int right = 1;
+	//for (int i = start - 1; i >= 0; --i) {
+	//	if (arr[start] > arr[i])
+	//		left = max(left, solution(i) + 1);
+	//}
+	//for (int i = start + 1; i < N; ++i) {
+	//	if (arr[start] < arr[i])
+	//		right = max(right, solution(i) + 1);
+	//}
+	//ret = left + right - 1;
 	return ret;
 }
 
@@ -34,8 +45,17 @@ int main()
 
 	int ret = 0;
 	for (int i = 0; i < N; ++i)
-		ret = max(ret, solution(i, true));
+		ret = max(ret, solution(i));
 
 	cout << ret;
 
 }
+
+
+/*
+아하 루프돌떄 자기 왼쪽으로는 점점 작아지게
+자기 오른쪽으로는 점점 크게해서 루프를 돌자!
+-> 무한 재귀
+
+
+*/
