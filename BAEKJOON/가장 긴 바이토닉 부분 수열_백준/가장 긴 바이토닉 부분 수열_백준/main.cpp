58272806d93e69269b96f200b0d4c29b2d32e9b5
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-05 21:21:55 +0900

    해결

--- BAEKJOON/가장 긴 바이토닉 부분 수열_백준/가장 긴 바이토닉 부분 수열_백준/main.cpp
+++ BAEKJOON/가장 긴 바이토닉 부분 수열_백준/가장 긴 바이토닉 부분 수열_백준/main.cpp
@@ -4,38 +4,42 @@ using namespace std;
 
 
 int arr[1000];
+int lefts[1000];
+int rights[1000];
 int cache[1000];
 int N = 0;
 
-int solution(int start)
+int leftnums(int start)
 {
-	int& ret = cache[start];
+	int& ret = lefts[start];
+	if (ret != 0)
+		return ret;
+	ret = 1;
+	for (int i = start - 1; i >= 0; --i) {
+
+		if (arr[start] > arr[i])
+			ret = max(ret, leftnums(i) + 1);
+
+	}
+	return ret;
+}
+
+int rightNums(int start)
+{
+	int& ret = rights[start];
 	if (ret != 0)
 		return ret;
 
+	ret = 1;
+	for (int i = start +1; i<N; ++i) {
 
-	for (int i = start + 1; i < N; ++i) {
-		if (true == order) {
-			if (arr[start] < arr[i])
-				ret = max(ret, solution(i, true) + 1);
-		}
-		if (arr[start] > arr[i])
-			ret = max(ret, solution(i, false) + 1);
+		if (arr[start]> arr[i])
+			ret = max(ret, rightNums(i) + 1);
 
 	}
-	//int left =1;
-	//int right = 1;
-	//for (int i = start - 1; i >= 0; --i) {
-	//	if (arr[start] > arr[i])
-	//		left = max(left, solution(i) + 1);
-	//}
-	//for (int i = start + 1; i < N; ++i) {
-	//	if (arr[start] < arr[i])
-	//		right = max(right, solution(i) + 1);
-	//}
-	//ret = left + right - 1;
 	return ret;
 }
+
 
 int main()
 {
@@ -44,9 +48,10 @@ int main()
 	for (int i = 0; i < N; ++i) cin >> arr[i];
 
 	int ret = 0;
-	for (int i = 0; i < N; ++i)
-		ret = max(ret, solution(i));
+	for (int i = 0; i < N; ++i){
+		ret = max(ret, leftnums(i)+rightNums(i)-1); //중복ㄱ 자기자신 빼주기
 
+	}
 	cout << ret;
 
 }
@@ -56,6 +61,13 @@ int main()
 아하 루프돌떄 자기 왼쪽으로는 점점 작아지게
 자기 오른쪽으로는 점점 크게해서 루프를 돌자!
 -> 무한 재귀
+
+
+이게 아니라
+ f(n)=f(k)+f(r);
+ k<n and n<r 이고
+arr[k] <arr[n] and arr[n]>arr[r]  
+이렇게 해야된다.
 
 
 */
