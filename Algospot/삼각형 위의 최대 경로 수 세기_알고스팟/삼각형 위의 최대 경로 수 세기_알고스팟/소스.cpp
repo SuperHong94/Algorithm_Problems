65d43fbd61c3e162355a99f511588d53581eddb1
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-06 16:38:07 +0900

    알고스팟 문제

---
+++ Algospot/삼각형 위의 최대 경로 수 세기_알고스팟/삼각형 위의 최대 경로 수 세기_알고스팟/소스.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int triangle[100][100];
+int cache[100][100];
+int N = 0;
+
+
+int solution(int x, int y)
+{
+	if (y == N || x == N)
+		return 0;
+	if (cache[y][x] != 0)
+		return cache[y][x];
+
+
+	int& ret = cache[y][x];
+	
+	return ret = max(triangle[y][x] + solution(x, y + 1), solution(x + 1, y + 1) + triangle[y][x]);
+
+}
+
+int main()
+{
+	int c = 0; cin >> c;
+	while (c--)
+	{
+		cin >> N;
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < i + 1; ++j) {
+				cin >> triangle[i][j];
+			}
+		}
+		int ret = solution(0, 0);
+
+		cout << ret << endl;
+	}
+
+
+}
