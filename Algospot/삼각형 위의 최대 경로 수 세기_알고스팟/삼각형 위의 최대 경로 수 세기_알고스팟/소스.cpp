62c01e911b4ae5190e4b0308b666b92123ac4147
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-07 15:07:30 +0900

    동적계획법+DFS로 했는데 시간초과 나온다.

--- Algospot/삼각형 위의 최대 경로 수 세기_알고스팟/삼각형 위의 최대 경로 수 세기_알고스팟/소스.cpp
+++ Algospot/삼각형 위의 최대 경로 수 세기_알고스팟/삼각형 위의 최대 경로 수 세기_알고스팟/소스.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <memory.h>
 using namespace std;
 
 int triangle[100][100];
 int cache[100][100];
-int N = 0;
 
+int N = 0;
+int MAX = 0;
+int cnt = 0;
 /*
-가장 큰 수를 구하는 것은 쉽다. 
+가장 큰 수를 구하는 것은 쉽다.
 문제는 가장 큰 수를 만드는 모든 경로를 세는 것?
 
 
@@ -21,9 +24,24 @@ int solution(int x, int y)
 
 
 	int& ret = cache[y][x];
-	
+
 	return ret = max(triangle[y][x] + solution(x, y + 1), solution(x + 1, y + 1) + triangle[y][x]);
 
+}
+
+void GetCourseCnt(int x, int y, int value)
+{
+	if (x == N)
+		return;
+	if (y == N - 1) {
+		value -= triangle[y][x];
+		if (value == 0)
+			cnt += 1;
+
+		return;
+	}
+	GetCourseCnt(x, y + 1, value - triangle[y][x]);
+	GetCourseCnt(x + 1, y + 1, value - triangle[y][x]);
 }
 
 int main()
@@ -31,16 +49,24 @@ int main()
 	int c = 0; cin >> c;
 	while (c--)
 	{
+		cnt = 0;
+		memset(cache, 0, sizeof(cache));
 		cin >> N;
 		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < i + 1; ++j) {
 				cin >> triangle[i][j];
 			}
 		}
-		int ret = solution(0, 0);
+		MAX = solution(0, 0);
+		GetCourseCnt(0, 0,MAX);
+		cout << cnt << endl;
 
-		cout << ret << endl;
+
+
 	}
 
 
 }
+
+
+//동적계획+DFS로는 시간초과 뜨네 ..
