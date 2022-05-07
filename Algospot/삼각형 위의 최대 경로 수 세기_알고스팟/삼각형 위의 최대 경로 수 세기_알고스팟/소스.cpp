1f31ae424afdc302b77e76770879401d44641e7f
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-07 21:34:09 +0900

    성공

--- Algospot/삼각형 위의 최대 경로 수 세기_알고스팟/삼각형 위의 최대 경로 수 세기_알고스팟/소스.cpp
+++ Algospot/삼각형 위의 최대 경로 수 세기_알고스팟/삼각형 위의 최대 경로 수 세기_알고스팟/소스.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int triangle[100][100];
 int cache[100][100];
+int cntCache[100][100];
 
 int N = 0;
-int MAX = 0;
-int cnt = 0;
+
 /*
 가장 큰 수를 구하는 것은 쉽다.
 문제는 가장 큰 수를 만드는 모든 경로를 세는 것?
@@ -29,19 +29,21 @@ int solution(int x, int y)
 
 }
 
-void GetCourseCnt(int x, int y, int value)
+int GetCourseCnt(int x, int y)
 {
-	if (x == N)
-		return;
-	if (y == N - 1) {
-		value -= triangle[y][x];
-		if (value == 0)
-			cnt += 1;
+	if (y == N-1)
+		return 1;
+	if (cntCache[y][x] != -1)
+		return cntCache[y][x];
 
-		return;
-	}
-	GetCourseCnt(x, y + 1, value - triangle[y][x]);
-	GetCourseCnt(x + 1, y + 1, value - triangle[y][x]);
+	int& ret = cntCache[y][x];
+	ret = 0;
+	if (cache[y + 1][x] < cache[y + 1][x + 1])
+		return ret += GetCourseCnt(x + 1, y + 1);
+	else if (cache[y + 1][x] > cache[y + 1][x + 1])
+		return ret += GetCourseCnt(x, y + 1);
+	else
+		return ret += GetCourseCnt(x, y + 1) + GetCourseCnt(x + 1, y + 1);
 }
 
 int main()
@@ -49,16 +51,17 @@ int main()
 	int c = 0; cin >> c;
 	while (c--)
 	{
-		cnt = 0;
+		
 		memset(cache, 0, sizeof(cache));
+		memset(cntCache, -1, sizeof(cntCache));
 		cin >> N;
 		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < i + 1; ++j) {
 				cin >> triangle[i][j];
 			}
 		}
-		MAX = solution(0, 0);
-		GetCourseCnt(0, 0,MAX);
+		solution(0, 0);
+		int cnt=GetCourseCnt(0, 0);
 		cout << cnt << endl;
 
 
@@ -69,4 +72,5 @@ int main()
 }
 
 
-//동적계획+DFS로는 시간초과 뜨네 ..
+
+
