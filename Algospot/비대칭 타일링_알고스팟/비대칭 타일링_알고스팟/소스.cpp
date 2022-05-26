c07206ff62515103abae0ccaf275025b14842837
SuperHong94 <ghdtnswh213@gmail.com>
2022-05-26 20:22:34 +0900

    힙구현해야함

--- Algospot/비대칭 타일링_알고스팟/비대칭 타일링_알고스팟/소스.cpp
+++ Algospot/비대칭 타일링_알고스팟/비대칭 타일링_알고스팟/소스.cpp
@@ -31,7 +31,7 @@ using namespace std;
 
 constexpr unsigned int MOD = 1000000007;
 unsigned long long cache[101];
-int half[51];
+
 int N = 0;
 
 int comp;
@@ -50,7 +50,7 @@ unsigned long long  sol3(int x)
 
 
 
-	return ret = sol3(x- 1) + sol3(x - 2);
+	return ret = (sol3(x - 1) + sol3(x - 2))%MOD;
 }
 
 int main()
@@ -59,70 +59,74 @@ int main()
 	while (C--)
 	{
 		memset(cache, -1, sizeof(cache));
-		memset(half, -1, sizeof(half));
+
 		cin >> N;
-		(N & 1) == 1 ? comp = N / 2 : comp = (N / 2) + 1;
-		int ret = (sol3(N)-sol3(comp))%MOD;
-		cout << ret << '\n';
+		int ret = 0;
+		if ((N & 1) == 1)
+			 ret = (sol3(N) - sol3(N/2)+MOD) % MOD;
+		else{
+			ret= (sol3(N) - sol3(N/2)+MOD) % MOD;
+			ret = (ret - sol3(N / 2 - 1) + MOD) % MOD;
+		}
+			cout << ret << '\n';
 	}
 }
 
 
-/*
-#include <iostream>
-#include <memory.h>
-using namespace std;
 
-constexpr int MOD = 1000000007;
-unsigned long long cache[101];
-unsigned long long  half[51];
-int N = 0;
-
-int comp;
-unsigned long long  sol(int x)
-{
-	if (x > N)
-		return 0;
-
-	unsigned long long& ret = cache[x];
-	if (ret != -1)
-		return ret;
-	if (x == N)
-		return 1;
-
-
-
-	return ret = sol(x + 1) + sol(x + 2);
-}
-
-unsigned long long   sol2(int x)
-{
-
-	if (x > comp)
-		return 0;
-
-	unsigned long long& ret = half[x];
-	if (ret != -1)
-		return ret;
-	if (x == comp)
-		return 1;
-
-
-	return ret = sol2(x + 1) + sol2(x + 2);
-}
-
-int main()
-{
-	int C = 0; cin >> C;
-	while (C--)
-	{
-		memset(cache, -1, sizeof(cache));
-		memset(half, -1, sizeof(half));
-		cin >> N;
-		(N & 1) == 1 ? comp = N / 2 : comp = (N / 2) + 1;
-		unsigned long long   ret = (sol(0) - sol2(0)) % MOD;
-		cout << ret << '\n';
-	}
-}
-
-*/
+//#include <iostream>
+//#include <memory.h>
+//using namespace std;
+//
+//constexpr int MOD = 1000000007;
+//unsigned long long cache[101];
+//unsigned long long  half[51];
+//int N = 0;
+//
+//int comp;
+//unsigned long long  sol(int x)
+//{
+//	if (x > N)
+//		return 0;
+//
+//	unsigned long long& ret = cache[x];
+//	if (ret != -1)
+//		return ret;
+//	if (x == N)
+//		return 1;
+//
+//
+//
+//	return ret = sol(x + 1) + sol(x + 2);
+//}
+//
+//unsigned long long   sol2(int x)
+//{
+//
+//	if (x > comp)
+//		return 0;
+//
+//	unsigned long long& ret = half[x];
+//	if (ret != -1)
+//		return ret;
+//	if (x == comp)
+//		return 1;
+//
+//
+//	return ret = sol2(x + 1) + sol2(x + 2);
+//}
+//
+//int main()
+//{
+//	int C = 0; cin >> C;
+//	while (C--)
+//	{
+//		memset(cache, -1, sizeof(cache));
+//		memset(half, -1, sizeof(half));
+//		cin >> N;
+//		(N & 1) == 1 ? comp = N / 2 : comp = (N / 2) + 1;
+//		unsigned long long   ret = (sol(0) - sol2(0)) % MOD;
+//		cout << ret << '\n';
+//	}
+//}
+//
--- BAEKJOON/가운데를 말해요_백준/가운데를 말해요_백준/소스.cpp
+++ BAEKJOON/가운데를 말해요_백준/가운데를 말해요_백준/소스.cpp
@@ -4,10 +4,39 @@ https://www.acmicpc.net/problem/1655
 
 */
 
+
+//자료구조에 정렬하면서 넣는다. 짝수일 경우 N/2 -1 index 출력
+//자료구조는 중복되야 하고 정렬을 유지해야 한다.
+
+
+
+//순서가 있는 자료구조 배열 그리고 index로 접근할 수 있는것
+//heap을 만들자.
+
+
+
 #include <iostream> 
+#include <vector>
+
 using namespace std;
+
+
+class HEAP
+{
+	vector<int> v;
+
+public:
+
+
+	void insert(int num)
+	{
+		if()
+	}
+};
+
 
 int main()
 {
+	int N; cin >> N;
 
 }
