b260d7f66f4a62d2f8ab84298b942b1bb8bd8bfb
SuperHong94 <ghdtnswh213@gmail.com>
2022-06-13 23:58:41 +0900

    폴리오미노 분석

--- Algospot/폴리오미노_알고스팟/폴리오미노_알고스팟/소스.cpp
+++ Algospot/폴리오미노_알고스팟/폴리오미노_알고스팟/소스.cpp
@@ -1,5 +1,38 @@
 /*
 https://algospot.com/judge/problem/read/POLY
+풀이
+
+세로 단조이므로 가로줄은 중간에 빈칸 없이 일렬로 있다.
+첫줄에 i개에 사각형이 있으면
+나머지디 n-i개의 사각형으로 폴리오미노를 만들고 첫줄에 적절히 붙이면 된다. 
+ex)
+i=2, n=4
+ㅁㅁ
+
+ㅁㅁㅁ
+ㅁ  
+
+이렇게 있으면
+ㅁㅁ
+ㅁㅁㅁ
+ㅁ
+
+  ㅁㅁ
+ㅁㅁㅁ
+ㅁ
+
+    ㅁㅁ
+ㅁㅁㅁ
+ㅁ
+
+ㅁㅁ
+  ㅁㅁㅁ
+  ㅁ
+
+
+이렇게 모형이 올 수 있다.
+그 경우의 수는 첫줄+두번째줄의 사각형 수 -1 이다.
+
 */
 
 #include <iostream>
@@ -8,6 +41,8 @@ using namespace std;
 
 constexpr int MOD = 10000000;
 int cache[101][101];//n개의 사각형일 때의 세로 단조 폴리오미노의 개수
+
+
 
 
 int sol(int n, int first)
