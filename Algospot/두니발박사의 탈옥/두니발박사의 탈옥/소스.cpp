706664b6150dd143d1360809ac43ed74ff905c62
SuperHong94 <ghdtnswh213@gmail.com>
2022-06-19 21:10:18 +0900

    박사 문제 진행중

--- Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
+++ Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
@@ -3,84 +3,44 @@ https://algospot.com/judge/problem/read/NUMB3RS
 */
 
 #include <iostream>
+#include <queue>
 using namespace std;
+char village[50][50];
+int village_cnt = 0;
+int day;
+int start;
+float result[100][50];//d에 n마을에 있을 확률 저장
 
-int main()
+void run(int d, int p)
 {
-	//
+	queue<int> q;
+	q.push(p);
+
+	while (!q.empty())
+	{
+		int DENOMINATOR = 0;
+		for (int i = 0; i < village_cnt; ++i)
+		{
+			if (village[p][i] == 0) continue;
+			q.push(i);
+			DENOMINATOR++;
+		}
+
+		for (int i = 0; i < village_cnt; ++i)
+		{
+			if (village[p][i] == 0) continue;
+			result[d][i] *= 1.0f / DENOMINATOR;
+		}
+
+
+	}
+
+
 }
-#pragma once
-#include<cstdio>
-#include<cstring>
-
-int adj[50][50]; int n, p;
-int deg[50];
-double adj2[50][50];
-double memoi[101][50];
-
-double dp[101][50];
-
 int main()
 {
-    int c;
-
-    scanf("%d\n", &c);
-
-    while (c--)
-    {
-        int d;
-
-        scanf("%d %d %d\n", &n, &d, &p);
-
-        memset(deg, 0, sizeof(deg));
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                scanf("%d \n", &adj[i][j]);
-                deg[i] += adj[i][j];
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                adj2[i][j] = (double)adj[i][j] / deg[i];
-            }
-        }
-
-        memset(dp, 0, sizeof(dp));
-
-        dp[0][p] = 1;
-
-        for (int days = 0; days < d; days++)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (dp[days][i] != 0)
-                {
-                    for (int j = 0; j < n; j++)
-                    {
-                        dp[days + 1][j] += dp[days][i] * adj2[i][j];
-                    }
-                }
-            }
-        }
-
-        int t;
-        scanf("%d\n", &t);
-
-        while (t--)
-        {
-            int q;
-            scanf("%d \n", &q);
-
-            printf("%.10lf ", dp[d][q]);
-        }
-
-        printf("\n");
-    }
-    return 0;
+	int c = 0; cin >> c;
+	while (c--) {
+		cin >> village_cnt >> day >> start;
+	}
 }
