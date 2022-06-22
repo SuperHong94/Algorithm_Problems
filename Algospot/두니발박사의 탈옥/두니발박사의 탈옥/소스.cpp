866b9b0204034c9bf4622738266ac38b1bd1be4c
SuperHong94 <ghdtnswh213@gmail.com>
2022-06-22 21:48:56 +0900

    다시 풀기

--- Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
+++ Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
@@ -14,15 +14,22 @@ int day;
 int start;
 double result[100][50];//d에 n마을에 있을 확률 저장
 
+bool isVisit[100][50];
+
 void BFS(const vector<vector<int>>& village, int d, int p, double percent)
 {
 	if (d > day)
 		return;
-	for (int i : village[p]) {
-		result[d][i] += (1.0f / village[p].size()) * percent;
 
-		BFS(village, d + 1, i, result[d][i]);
-	}
+	if (isVisit[d][p] == false)
+		for (int i : village[p]) {
+			result[d][i] += (1.0f / village[p].size()) * percent;
+
+
+			BFS(village, d + 1, i, result[d][i]);
+			isVisit[d][p] = true;
+		}
+
 
 }
 
@@ -34,6 +41,7 @@ int main()
 	int c = 0; cin >> c;
 	while (c--) {
 		memset(result, 0, sizeof(result));
+		memset(isVisit, 0, sizeof(isVisit));
 		cin >> village_cnt >> day >> start;
 		vector<vector<int>> village(village_cnt);
 		for (int i = 0; i < village_cnt; ++i)
@@ -48,9 +56,9 @@ int main()
 			int a = 0; cin >> a;
 			q.push_back(a);
 		}
-			BFS(village, 1, start, 1.0f);
+		BFS(village, 1, start, 1.0f);
 
-		for (int item:q)
+		for (int item : q)
 			cout << result[day][item] << ' ';
 		cout << endl;
 
