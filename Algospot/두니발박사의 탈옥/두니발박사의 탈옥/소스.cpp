c0d471b85f540cb2a9eb8c0a954e17ae127caac1
SuperHong94 <ghdtnswh213@gmail.com>
2022-06-24 21:18:11 +0900

    두니발박사 완료

--- Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
+++ Algospot/두니발박사의 탈옥/두니발박사의 탈옥/소스.cpp
@@ -1,5 +1,7 @@
 /*
 https://algospot.com/judge/problem/read/NUMB3RS
+
+생각을 바꿔서 애초에 d일 에 해당마을에서 시작마을로 가는 확률을 구해볼까?
 */
 
 #include <iostream>
@@ -12,23 +14,47 @@ using namespace std;
 int village_cnt = 0;
 int day;
 int start;
-double result[100][50];//d에 n마을에 있을 확률 저장
+double result[101][51];//d에 n마을에 있을 확률 저장
 
-bool isVisit[100][50];
 
-void BFS(const vector<vector<int>>& village, int d, int p, double percent)
+//void BFS(const vector<vector<int>>& village, int d, int p, double percent)
+//{
+//	if (d > day)
+//		return;
+//
+//	if (isVisit[d][p] == false)
+//		for (int i : village[p]) {
+//			result[d][i] += (1.0f / village[p].size()) * percent;
+//
+//
+//			BFS(village, d + 1, i, result[d][i]);
+//			isVisit[d][p] = true;
+//		}
+//
+//
+//}
+
+double BFS2(const vector<vector<int>>& village, int d, int pos)
 {
-	if (d > day)
-		return;
+	if (d == 0) {
+		if (pos == start)
+			return 1.0;
+		else
+			return 0.0;
+	}
 
-	if (isVisit[d][p] == false)
-		for (int i : village[p]) {
-			result[d][i] += (1.0f / village[p].size()) * percent;
-
-
-			BFS(village, d + 1, i, result[d][i]);
-			isVisit[d][p] = true;
+		if (result[d][pos] != -1.0)
+			return result[d][pos];
+		
+		result[d][pos] = 0.0;
+		for (int i : village[pos])
+		{
+			result[d][pos] += BFS2(village, d - 1, i) / village[i].size();
 		}
+
+		return result[d][pos];
+	
+
 
 
 }
@@ -40,8 +66,7 @@ int main()
 {
 	int c = 0; cin >> c;
 	while (c--) {
-		memset(result, 0, sizeof(result));
-		memset(isVisit, 0, sizeof(isVisit));
+
 		cin >> village_cnt >> day >> start;
 		vector<vector<int>> village(village_cnt);
 		for (int i = 0; i < village_cnt; ++i)
@@ -50,16 +75,22 @@ int main()
 				if (a == 0)continue;
 				village[i].push_back(j);
 			}
+		for (int i = 0; i <= day; ++i)
+		{
+			for (int j = 0; j <= village_cnt; ++j)
+				result[i][j] = -1.0;
+		}
 		int t = 0; cin >> t;
-		vector<int> q;
+
 		for (int i = 0; i < t; ++i) {
 			int a = 0; cin >> a;
-			q.push_back(a);
+			cout.precision(8);
+			cout << BFS2(village, day, a) << ' ';
 		}
-		BFS(village, 1, start, 1.0f);
 
-		for (int item : q)
-			cout << result[day][item] << ' ';
+
+
+	
 		cout << endl;
 
 
