7bfab2ace4e600bd5f0636eca8315dd501133183
SuperHong94 <ghdtnswh213@gmail.com>
2022-06-28 22:05:04 +0900

    완료

--- Programmers/순위_프로그래머스/순위_프로그래머스/소스.cpp
+++ Programmers/순위_프로그래머스/순위_프로그래머스/소스.cpp
@@ -1,37 +1,36 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <iostream>
+#include <cstring>
 using namespace std;
+bool graph[101][101];
 
-struct Player
-{
-	set<int> win; //승리한 플레이어
-	set<int> lose;//진 플레이어
-};
 int solution(int n, vector<vector<int>> results) {
-	int answer = 0;
-	vector<Player> players;
-	players.resize(results.size() + 1);
+    int answer = 0;
+    for (int i = 0; i < results.size(); i++) {
+        graph[results[i][0]][results[i][1]] = true;
+    }
 
-	for (auto& result : results) {
-		int winPlayer = result[0];
-		int losePlayer = result[1];
-		players[winPlayer].win.insert(losePlayer);
-		for (int d : players[losePlayer].win)  //진플레이어가 이긴 모든 플레이어는 승리플레이어도 이긴다.
-			players[winPlayer].win.insert(d);
 
-		players[losePlayer].lose.insert(winPlayer);
-		for (int d : players[winPlayer].lose) //승리플레이어가 패배한 모든플레이어는 진플레이어도 진다.
-			players[losePlayer].lose.insert(d);
-	}
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (graph[i][k] && graph[k][j]) {
+                    graph[i][j] = true;
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        int count = 0;
+        for (int j = 1; j <= n; j++) {
+            if (graph[i][j] || graph[j][i]) {
+                count++;
+            }
+        }
+        if (count == n - 1) {
+            answer++;
+        }
+    }
 
-	for (int i = 1; i <= n; ++i) {
-
-		for (int win = 0; win < n; ++win) {
-			if (players[i].win.size()==win && players[i].lose.size() == n-win-1)
-				answer++;
-		}
-	}
-	return answer;
+    return answer;
 }
