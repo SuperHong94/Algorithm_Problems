02615319a6253acb72db527f4a6deda0ca34b849
SuperHong94 <ghdtnswh213@gmail.com>
2022-07-05 23:52:12 +0900

    지금까지한거

--- Algospot/여행 짐 싸기_알고스팟/여행 짐 싸기_알고스팟/소스.cpp
+++ Algospot/여행 짐 싸기_알고스팟/여행 짐 싸기_알고스팟/소스.cpp
@@ -1,4 +1,5 @@
 //백준 평범한 배낭부터 다시 도전해본다.
+//https://www.acmicpc.net/problem/12865
 
 #include <iostream>
 #include <vector>
@@ -7,6 +8,18 @@ using namespace std;
 int N = 0, K = 0;
 
 vector<pair<int, int>> objects;
+bool visit[100];
+int sol()
+{
+
+	for (int i = 0; i < objects.size(); ++i)
+	{
+		int w = objects[i].first;
+		int v = objects[i].second;
+
+
+	}
+}
 int main()
 {
 	cin >> N >> K;
