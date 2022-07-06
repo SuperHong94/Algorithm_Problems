8b673ce3f0506500bd405ac4ded6c0ccbb1c3336
SuperHong94 <ghdtnswh213@gmail.com>
2022-07-06 23:09:21 +0900

    다시풀기

--- Algospot/여행 짐 싸기_알고스팟/여행 짐 싸기_알고스팟/소스.cpp
+++ Algospot/여행 짐 싸기_알고스팟/여행 짐 싸기_알고스팟/소스.cpp
@@ -3,31 +3,49 @@
 
 #include <iostream>
 #include <vector>
+#include <memory.h>
 using namespace std;
 
 int N = 0, K = 0;
 
 vector<pair<int, int>> objects;
 bool visit[100];
-int sol()
+int cache[101][100001];
+int sol(int index,int weight)
 {
+	if (weight > K)
+		return 0;
+	int& ret = cache[index][weight];
+	if (ret != -1)
+		return ret;
 
+	ret = 0;
 	for (int i = 0; i < objects.size(); ++i)
 	{
-		int w = objects[i].first;
+		if (visit[i] == true)continue;
+		int w = objects[i].first+weight;
+		if (w > K)
+			continue;
 		int v = objects[i].second;
-
-
+		visit[i] = true;
+		ret = max(ret, sol(i,w)+v);
+		visit[i] = false;
 	}
+
+	return ret;
+
+
 }
 int main()
 {
 	cin >> N >> K;
 	int n = N;
+	memset(cache, -1, sizeof(cache));
 	while (n--)
 	{
 		int w = 0, v = 0; cin >> w >> v;
 		objects.emplace_back( w,v );
 
 	}
+	cout << sol(0,0) << endl;
 }
