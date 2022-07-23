b6dc28069b954b396471d5203c22df1256d3bcd1
SuperHong94 <ghdtnswh213@gmail.com>
2022-07-23 15:38:14 +0900

    데우는시간이 최소가 되는것 선택하면 안된다.

--- Algospot/도시락 데우기_알고스팟/도시락 데우기_알고스팟/소스.cpp
+++ Algospot/도시락 데우기_알고스팟/도시락 데우기_알고스팟/소스.cpp
@@ -7,87 +7,77 @@
 #include <vector>
 using namespace std;
 
-int waveTimes[10000];
+//int waveTimes[10000];
 int eatTimes[10000];
 int N = 0;
 
 
-//제일짧은 waveTime 뽑기
+//제일짧은 waveTime 뽑기 단순히 이렇게하면안됨 다먹는시간이 최소가 되는것이 정답이다.
 //밥먹기 (wave끝났으면)
 
-int GeMinWaveIndex()
-{
-	int MIN = 12345679;
-	int index = -1;
-	for (int i = 0; i < N; ++i) {
-		if (waveTimes[i] == 0)continue;
-		if (MIN > waveTimes[i]) {
-			MIN = waveTimes[i];
-			index = i;
-		}
-	}
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> waveTimes;
 
-	return index;
-}
+//int GeMinWaveIndex()
+//{
+//	int MIN = 12345679;
+//	int index = -1;
+//	for (int i = 0; i < N; ++i) {
+//		if (waveTimes[i] == 0)continue;
+//		if (MIN > waveTimes[i]) {
+//			MIN = waveTimes[i];
+//			index = i;
+//		}
+//	}
+//
+//	return index;
+//}
 
 int main()
 {
 	int C = 0; cin >> C;
 
-
-
-	/*
-	------
-	 - -- -
-	*/
 	while (C--)
 	{
 		cin >> N;
 
-		for (int j = 0; j < N; ++j)
-			cin >> waveTimes[j];
+		for (int j = 0; j < N; ++j) {
+			int time;
+			cin >> time;
+
+			waveTimes.push({ time,j });
+		}
+
+
 		for (int j = 0; j < N; ++j)
 			cin >> eatTimes[j];
 
 
 		vector<int> eatIndex;
 		int totalTime = 0;
-		while (true)
+		while (waveTimes.empty() == false)
 		{
-			int index = GeMinWaveIndex();
-			int breakCnt = 0;
-			int minusTime = 1;
-			if (index != -1) {
-				minusTime = waveTimes[index];
-				waveTimes[index] = waveTimes[index] - minusTime;
 
-				for (int index : eatIndex)
-				{
-					if (eatTimes[index] <= 0) {
-						breakCnt++;
-						continue;
-					}
-					eatTimes[index] -= minusTime;
-				}
-				eatIndex.push_back(index);
+			auto item = waveTimes.top();
+			waveTimes.pop();
+			int waveTime = item.first;
+			int index = item.second;
+			totalTime += waveTime;
+
+			for (auto i : eatIndex)
+			{
+				if (eatTimes <= 0)continue;
+				eatTimes[i] -= waveTime;
 			}
-			else {
-				for (int index : eatIndex)
-				{
-					if (eatTimes[index] <= 0) {
-						breakCnt++;
-						continue;
-					}
-					eatTimes[index] -= minusTime;
-				}
-			}
-		
-		
-			if (breakCnt == N)
+			eatIndex.push_back(index);
+		}
+
+		for (auto i : eatIndex)
+		{
+			if (eatTimes[i] > 0)
+			{
+				totalTime += eatTimes[i];
 				break;
-
-		
-			totalTime += minusTime;
+			}
 		}
 
 		cout << totalTime << endl;
