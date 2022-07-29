6988ad68fab67e143adc79a2c810e90e07a6a555
SuperHong94 <ghdtnswh213@gmail.com>
2022-07-29 18:30:22 +0900

    완료

--- BAEKJOON/피사노주기/피사노주기/main.cpp
+++ BAEKJOON/피사노주기/피사노주기/main.cpp
@@ -6,5 +6,25 @@ using namespace std;
 
 int main()
 {
+	int T = 0; cin >> T;
+	while (T--)
+	{
+		long long N = 0, M; cin >> N >> M;
 
+		long long p = 1, pp = 0, cur = 0;
+
+		long long ret = 0;
+		for (long long j = 2;; ++j) {
+			cur = (p % M + pp % M) % M;
+			if (cur == 0 && p == 1) {
+				ret = j;
+				break;
+			}
+			pp = p;
+			p = cur;
+
+
+		}
+		cout << N << ' ' << ret << '\n';
+	}
 }
