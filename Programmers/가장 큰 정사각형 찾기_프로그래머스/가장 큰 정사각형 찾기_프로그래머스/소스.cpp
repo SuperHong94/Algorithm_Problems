c23c93c2e98067f77ca2458b2f97475c4627d091
SuperHong94 <ghdtnswh213@gmail.com>
2022-07-31 16:58:34 +0900

    가장 큰 정사각형 완료

--- Programmers/가장 큰 정사각형 찾기_프로그래머스/가장 큰 정사각형 찾기_프로그래머스/소스.cpp
+++ Programmers/가장 큰 정사각형 찾기_프로그래머스/가장 큰 정사각형 찾기_프로그래머스/소스.cpp
@@ -6,35 +6,46 @@
 using namespace std;
 
 int R = 0, C = 0;
-int GetMaxArea(int y, int x, const vector<vector<int>>& board) //이걸로 풀면 효율성 탈락함
+
+
+int cache[1000][1000];
+int DFS(int y, int x,const vector<vector<int>>& board)
 {
-    int area = 0;
-    for (int i = 1;; ++i)
-    {
-        for (int dy = 0; dy < i; ++dy) {
-            for (int dx = 0; dx < i; ++dx) {
-                if (dy + y >= R || dx + x >= C)
-                    return area;
-                if (board[dy + y][dx + x] == 0)
-                    return area;
-            }
-        }
-        area = i * i;
-    }
+    if (y < 0 || y >= R || x < 0 || x >= C)
+        return 0;
+
+    int& ret = cache[y][x];
+    if (cache[y][x] != -1)
+        return cache[y][x];
+  
+    if (board[y][x] == 0)
+        return ret =0;
+    ret = 1;
+    int a = DFS(y + 1, x, board),b= DFS(y, x + 1, board),c= DFS(y + 1, x + 1, board);
+
+    
+    int MIN= min(a, b);
+    MIN = min(MIN, c);
+
+    return ret = MIN+1;
 
 }
 
 int solution(vector<vector<int>> board)
 {
+    memset(cache, -1, sizeof(cache));
     int answer = 1234;
     R = board.size(); C = board[0].size();
     int MAX = 0;
     for (int i = 0; i < R; ++i)
     {
         for (int j = 0; j < C; ++j) {
-            MAX = max(MAX, GetMaxArea(i, j, board));
+            int w = DFS(i, j, board);
+            MAX = max(MAX, w*w);
         }
     }
 
     return MAX;
 }
+
+
