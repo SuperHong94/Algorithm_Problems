c60f27513ee2de6df34f5c3a04a53ae84e0bc53c
SuperHong94 <ghdtnswh213@gmail.com>
2022-08-05 21:20:35 +0900

    문자열 합치기 완료

--- Algospot/문자열 합치기_알고스팟/문자열 합치기_알고스팟/소스.cpp
+++ Algospot/문자열 합치기_알고스팟/문자열 합치기_알고스팟/소스.cpp
@@ -1,13 +1,37 @@
+//https://algospot.com/judge/problem/read/STRJOIN
 #include <iostream>
-
+#include <vector>
+#include <queue>
 using namespace std;
 
+int strLen[1000];
+int N = 0;
 int main()
 {
 	int T = 0; cin >> T;
 
 	while (T--)
 	{
+		priority_queue<int, vector<int>, greater<int>> pq;
 
+		cin >> N;
+		for (int i = 0; i < N; ++i) {
+			int a = 0;
+			cin >> a;
+			pq.push(a);
+		}
+		int result = 0;
+		while (pq.empty() == false) {
+			while (pq.size() > 1) {
+				int a = pq.top(); pq.pop();
+				int b = pq.top(); pq.pop();
+				pq.push(a + b);
+				result += (a + b);
+			}
+			if (pq.size() == 1)
+				break;
+
+		}
+		cout << result << '\n';
 	}
 }
