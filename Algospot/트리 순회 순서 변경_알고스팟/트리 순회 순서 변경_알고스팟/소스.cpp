11eb797baf7465fb6c38280da976fa7ef5e8adf9
SuperHong94 <ghdtnswh213@gmail.com>
2022-09-10 22:12:31 +0900

    순회문제

---
+++ Algospot/트리 순회 순서 변경_알고스팟/트리 순회 순서 변경_알고스팟/소스.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+class Node
+{
+	int val;
+	Node* left;
+	Node* right;
+public:
+	Node(int n) :val(n) { left = nullptr; right = nullptr; }
+
+	void insert(int n)
+	{
+		if (n < val)
+		{
+			if (left == nullptr)
+				left = new Node(n);
+			else
+				left->insert(n);
+		}
+		else {
+			if (right == nullptr)
+				right = new Node(n);
+			else
+				right->insert(n);
+		}
+	}
+
+	void postOrder()
+	{
+		if (left != nullptr)
+			left->postOrder();
+		if (right != nullptr)
+			right->postOrder();
+		cout << val << ' ';
+	}
+
+};
+
+void postOrder(const vector<int>& pre, const vector<int>& mid)
+{
+	int n = pre.size();
+	if (n == 0)
+		return;
+	int root = pre[0];
+	int m = find(mid.begin(), mid.end(), root)-mid.begin();
+	postOrder(vector<int>(mid.begin()+1,mid.begin()+m))
+
+}
+int main()
+{
+	int T = 0; cin >> T;
+	while (T--)
+	{
+		int n = 0; cin >> n;
+		vector<int> preOrder;
+		vector<int> midOrder;
+		int a = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> a;
+			preOrder.push_back(a);
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> a;
+			midOrder.push_back(a);
+		}
+
+
+	}
+}
