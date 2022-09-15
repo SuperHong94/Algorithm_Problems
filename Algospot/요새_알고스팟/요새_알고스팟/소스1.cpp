44d3e167dfab6147419b7478f1ea956d92c76906
SuperHong94 <ghdtnswh213@gmail.com>
2022-09-16 00:56:29 +0900

    요새 클리어!

--- Algospot/요새_알고스팟/요새_알고스팟/소스1.cpp
+++ Algospot/요새_알고스팟/요새_알고스팟/소스1.cpp
@@ -1,85 +1,95 @@
-//#include <iostream>
-//#include<algorithm>
-//#include<cmath>
-//#include<vector>
-//using namespace std;
-//
-//class CastleNode {
-//public:
-//    int x, y, r;
-//    vector<CastleNode*> children;
-//
-//    CastleNode(int _x, int _y, int _r) : x(_x), y(_y), r(_r) {}
-//
-//    void AddChild(CastleNode* b) {
-//        CastleNode* a;
-//
-//        for (int i = 0; i < children.size(); ++i) {
-//            a = children[i];
-//            if (pow(a->r, 2) > pow((a->x - b->x), 2) + pow((a->y - b->y), 2) + pow(b->r, 2)) {
-//                children[i]->AddChild(b);
-//                return;
-//            }
-//
-//        }
-//        children.push_back(b);
-//    }
-//
-//    bool operator<(const CastleNode& cas) const {
-//        return (this->r) > (cas.r);
-//    }
-//
-//};
-//
-//int castle_num, longest;
-//vector<CastleNode> castles;
-//
-//int TreeHeigth(CastleNode* cas) {
-//    vector<int> heights;
-//    int max_height = 0;
-//
-//    if (cas->children.size() == 0)
-//        return 0;
-//
-//    for (int i = 0; i < cas->children.size(); ++i) {
-//        heights.push_back(TreeHeigth(cas->children[i]) + 1);
-//        max_height = max(max_height, heights.back());
-//    }
-//
-//    if (cas->children.size() > 1) {
-//        sort(heights.begin(), heights.end());
-//        longest = max(longest, heights[heights.size() - 1] + heights[heights.size() - 2]);
-//    }
-//
-//    return max_height;
-//}
-//
-//int main() {
-//    int test_num;
-//    cin >> test_num;
-//
-//    int x, y, r;
-//    CastleNode* root;
-//    while (test_num--) {
-//        cin >> castle_num;
-//
-//        for (int i = 0; i < castle_num; ++i) {
-//            cin >> x >> y >> r;
-//            castles.push_back(CastleNode(x, y, r));
-//        }
-//
-//        sort(castles.begin(), castles.end());
-//
-//        root = &castles[0];
-//        for (int i = 1; i < castle_num; ++i)
-//            root->AddChild(&castles[i]);
-//
-//        longest = 0;
-//        int root_height = TreeHeigth(root);
-//        cout << max(root_height, longest) << "\n";
-//
-//        castles.clear();
-//    }
-//
-//    return 0;
-//}
+//https://algospot.com/judge/problem/read/FORTRESS
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+class Wall
+{
+public:
+	int x, y, r;
+	Wall(int _x, int _y, int _r) :x(_x), y(_y), r(_r) {}
+
+	vector<Wall*> children; //포함되는녀석
+
+	bool operator<(const Wall& right)const
+	{
+		return r < right.r;
+	}
+
+
+
+};
+
+
+int longest;
+int height(Wall* root)
+{
+	vector<int> heights;
+	for (int i = 0; i < root->children.size();++i)
+	{
+		heights.push_back(height(root->children[i]));
+	}
+	if (heights.empty())return 0;
+	sort(heights.begin(), heights.end());
+	if (heights.size() >= 2)
+		longest = max(longest, 2 + heights[heights.size() - 2] + heights[heights.size() - 1]);
+
+	return heights.back() + 1;
+}
+
+
+bool IsContain(const Wall& a, const Wall& b) //a가 b에 포함되는가?
+{
+	int dx = a.x - b.x;
+	int dy = a.y - b.y;
+	int r = b.r;
+	dx = dx * dx;
+	dy = dy * dy;
+	r = r * r;
+
+	return dx + dy < r;
+
+
+}
+
+
+int main()
+{
+	int T = 0; cin >> T;
+
+	while (T--)
+	{
+		int N = 0; cin >> N;
+
+		int x = 0, y = 0, r = 0;
+		vector<Wall> v;
+		for (int i = 0; i < N; ++i)
+		{
+			cin >> x >> y >> r;
+			v.push_back({ x,y,r });
+		}
+		sort(v.begin(), v.end());
+
+		for (int i = 0; i < N - 1; ++i)
+		{
+			for (int j = i + 1; j < N; ++j)
+			{
+				if (true == IsContain(v[i], v[j]))
+				{
+					v[j].children.push_back(&v[i]);
+					break;
+				}
+			}
+		}
+		longest = 0;
+		int h = height(&v[N - 1]);
+		
+		cout << max(longest, h) << '\n';
+
+
+
+	}
+
+}
